83e30afdb3670b0421044b0f6eee84d2b82264ab
HK-Productions <161631574+HK-Productions@users.noreply.github.com>
2024-12-12 13:16:18 +0530

    Update premium.py

--- plugins/Extra/premium.py
+++ plugins/Extra/premium.py
@@ -1,3 +1,16 @@
+from datetime import timedelta
+from asyncio import sleep 
+import pytz
+import datetime, time
+from info import ADMINS, LOG_CHANNEL, PAYMENT_QR , OWNER_UPI_ID
+from Script import script 
+from utils import get_seconds, get_status, temp
+from database.users_chats_db import db 
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong
+import traceback
+
 @Client.on_message(filters.command("bought") & filters.private)
 async def bought(client, message):
     msg = await message.reply('Wait im checking...')
@@ -7,7 +20,7 @@ async def bought(client, message):
     if replyed and replyed.photo:
         await client.send_photo(
             photo=replyed.photo.file_id,
-            chat_id=UPI_PAY_LOGS,
+            chat_id=LOG_CHANNEL,
             caption=f'<b>User - {message.from_user.mention}\nUser id - <code>{message.from_user.id}</code>\nusername - <code>{message.from_user.username}</code>\nUser Name - <code>{message.from_user.first_name}</code></b>',
             reply_markup=InlineKeyboardMarkup(
                 [
